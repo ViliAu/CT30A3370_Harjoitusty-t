10eee2be816c0e6d532fc3aa2d1c063d137aa0e7
jani-heinikoski <heinikoski.jani@gmail.com>
2021-02-19 17:42:01 +0200

    Reverse lähdekooditiedosto luotu

---
+++ Project_1/reverse.c
@@ -0,0 +1,13 @@
+/*
+Authors: Jani Heinikoski, Vili Huusko
+Last modified: 19.02.2021
+Sources: -
+*/
+#include "reverse.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (void) {
+    return 0;
+}
