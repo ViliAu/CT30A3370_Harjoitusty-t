c42ad0b4ee5cd29ed75e0676aab03d55ca9104fa
jani-heinikoski <heinikoski.jani@gmail.com>
2021-02-19 19:18:56 +0200

    Yksikkötestejä dynaamiselle muistille. free_node, push ja new_node yksikkötestattu

--- Project_1/reverse.c
+++ Project_1/reverse.c
@@ -5,12 +5,29 @@ Sources: -
 */
 #include "reverse.h"
 
+/* UNIT TESTS, LATER MOVED TO SEPARATE FILE */
+
+void test_new_node() {
+    Node* n = new_node();
+    if (n == NULL || n->line != NULL || n->next != NULL) {
+        fprintf(stderr, "error: new_node unit test failed\n");
+    }
+}
+
+void test_push() {
+    /* NULL arg tests */
+    push(NULL, NULL);
+    push(NULL, new_node());
+    Node* stack_head = new_node();
+    push(&stack_head, NULL);
+}
+
 Node* new_node() {
     /* Used to malloc Nodes for the linked list */
     Node* n = malloc(sizeof(Node));
 
     if (n == NULL) {
-        fprintf(stderr, "error: malloc failed");
+        fprintf(stderr, "error: malloc failed\n");
         exit(1);
     }
 
@@ -25,15 +42,17 @@ void push(Node** stack_head, Node* node) {
     if (stack_head == NULL || node == NULL) {
         return;
     }
-    Node* tmp = *stack_head;
     node->next = *stack_head;
     *stack_head = node;
 }
 
 void free_node(Node* node, bool line_needs_free) {
     /* Free's the malloc'd node */
-    if (line_needs_free == true) {
-        free(node->line);
+    if (node == NULL) {
+        return;
+    }
+    if (line_needs_free == true && node->line != NULL) {
+        free((node->line));
     }
     free(node);
 }
