142ad09319b8663eb8b3227b2f50541bfd1cc171
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2021-02-19 15:29:55 +0200

    Create reverse.h

---
+++ Project_1/reverse.h
@@ -0,0 +1,22 @@
+/*
+Authors: Jani Heinikoski, Vili Huusko
+Last modified: 19.02.2021
+Sources: -
+*/
+#ifndef REVERSE
+#define REVERSE
+/* Stack structure's node */
+typedef struct Node {
+    char* line;
+    struct Node* next;
+} Node;
+/* Dynamic memory */
+Node* new_node();
+void push(Node** node, Node** stack_head);
+Node* pop(Node** stack_head);
+void free_node(Node** node);
+/* File reading */
+char** read_lines(FILE* file_stream);
+/* File writing */
+void write(FILE* file_stream, char** lines);
+#endif
