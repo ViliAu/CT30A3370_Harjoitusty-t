fc50acd57ae3ced7bc9f03d55e609afa45cf2c22
jani-heinikoski <heinikoski.jani@gmail.com>
2021-02-19 18:44:45 +0200

    Push funktion määrittely, ei testattu

--- Project_1/reverse.c
+++ Project_1/reverse.c
@@ -5,7 +5,6 @@ Sources: -
 */
 #include "reverse.h"
 
-
 Node* new_node() {
     /* Used to malloc Nodes for the linked list */
     Node* n = malloc(sizeof(Node));
@@ -19,6 +18,16 @@ Node* new_node() {
     n->next = NULL;
 
     return n;
+}
+
+void push(Node** stack_head, Node* node) {
+    /* Pushes a new node to the stack */
+    if (stack_head == NULL || node == NULL) {
+        return;
+    }
+    Node* tmp = *stack_head;
+    node->next = *stack_head;
+    *stack_head = node;
 }
 
 void free_node(Node* node, bool line_needs_free) {
