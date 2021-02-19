86297ebde1706b023831203b0e8f1756dae25c8d
jani-heinikoski <heinikoski.jani@gmail.com>
2021-02-19 18:02:04 +0200

    new_node funktio (ei testattu)

--- Project_1/reverse.c
+++ Project_1/reverse.c
@@ -5,6 +5,22 @@ Sources: -
 */
 #include "reverse.h"
 
+
+Node* new_node() {
+    /* Used to malloc Nodes for the linked list */
+    Node* n = malloc(sizeof(Node));
+
+    if (n == NULL) {
+        fprintf(stderr, "error: malloc failed");
+        exit(1);
+    }
+
+    n->line = NULL;
+    n->next = NULL;
+
+    return n;
+}
+
 int main (void) {
     return 0;
 }
