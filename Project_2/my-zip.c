4369b58dbdb666a79962783370e9499b0787509a
jani-heinikoski <heinikoski.jani@gmail.com>
2021-04-15 12:12:45 +0300

    Temporary includes and definitions

---
+++ Project_2/my-zip.c
@@ -0,0 +1,13 @@
+/*
+Authors: Jani Heinikoski, Vili Huusko
+Last modified (date): 15.04.2021
+Sources:
+- fwrite man page
+*/
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define READ_BINARY "rb"
+#define WRITE_BINARY "wb"
