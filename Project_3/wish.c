92329f54c0599efe65ea16f447f5689ea1af1039
jani-heinikoski <heinikoski.jani@gmail.com>
2021-04-24 20:53:33 +0300

    reorganized code and added comments

--- Project_3/wish.c
+++ Project_3/wish.c
@@ -1,3 +1,8 @@
+/*
+Authors: Jani Heinikoski, Vili Huusko
+Last modified: 24.04.2021
+Sources: -
+*/
 #define _GNU_SOURCE
 
 #include <unistd.h>
@@ -37,6 +42,8 @@ void increment_str_size(char**);
 void tokenize(Token**, char*);
 void add_token(Token**, Token*);
 bool supported_by_ascii(int);
+
+/* ****************************** TEST FUNCTIONS ****************************** */
 /* For test purposes only */
 void print_list(Token*);
 /* For test purposes only */
@@ -47,63 +54,9 @@ void print_list(Token* head) {
         ptr = ptr->next;
     }
 }
+/* ****************************** UTILITY FUNCTIONS ****************************** */
 
-bool supported_by_ascii(int c) {
-    return (c >= 0 && c <= 127);
-}
-
-Token* new_token() {
-    Token* token = malloc(sizeof(Token));
-    if (!token) {
-        on_error(MALLOC_ERR_MSG, true);
-    }
-    token->token_length = 0;
-    token->token = NULL;
-    token->next = NULL;
-    return token;
-}
-
-void free_list(Token* head) {
-    Token* ptr = head;
-    while(ptr) {
-        head = ptr;
-        ptr = ptr->next;
-        free(head->token);
-        free(head);
-    }
-}
-
-/* Wrapper with error handling for fopen */
-FILE* open_file(char* filename, char* mode) {
-    FILE* fp = fopen(filename, mode);
-    if (!fp) {
-        on_error(OPEN_FILE_ERR_MSG, true);
-    }
-    return fp;
-}
-
-/* Reads a whole line from input_stream to line, returns false in case of EOF */
-bool get_line(char** line, FILE* input_stream) {
-    if (!input_stream) {
-        on_error(GET_LINE_ERR_MSG, true);
-    }
-    *line = NULL;
-    size_t size_of_line = 0;
-    ssize_t characters_read = getline(line, &size_of_line, input_stream);
-    if (characters_read == -1) {
-        if (ferror(input_stream) != 0) {
-            /* Global errno is set if getline fails and returns -1, also returns -1 on EOF.
-             According to man pages, *line should be free'd anyway. */
-            on_error(GET_LINE_ERR_MSG, false);
-            free(*line);
-            exit(1);
-        }
-        free(*line);
-        return false;
-    }
-    return true;
-}
-
+/* Called whenever an error is detected, prints default err msg and arg error_message to stderr */
 void on_error(char* error_message, bool exit_after) {
     write(STDERR_FILENO, STD_ERR_MSG, strlen(STD_ERR_MSG));
     if (error_message) {
@@ -112,25 +65,7 @@ void on_error(char* error_message, bool exit_after) {
     if (exit_after) exit(1);
 }
 
-char* new_str(size_t str_length) {
-    char* str = malloc(str_length);
-    if (!str) {
-        on_error(MALLOC_ERR_MSG, false);
-        exit(1);
-    }
-    *str = '\0';
-    return str;
-}
-
-void increment_str_size(char** str) {
-    size_t new_size = strlen(*str) + 2;
-    *str = realloc(*str, new_size);
-    if (!(*str)) {
-        on_error(MALLOC_ERR_MSG, false);
-        exit(1);
-    }
-}
-
+/* Validates read input, does not guarantee correct commands but checks for syntax errors */
 bool validate_input(char* input, Token** head) {
     if (!input) return false;
     size_t input_length = strlen(input);
@@ -140,18 +75,11 @@ bool validate_input(char* input, Token** head) {
     return true;
 }
 
-void add_token(Token** head, Token* tok) {
-    if (*head == NULL) {
-        *head = tok;
-        return;
-    }
-    Token* ptr = *head;
-    while (ptr->next != NULL) {
-        ptr = ptr->next;
-    }
-    ptr->next = tok;
+bool supported_by_ascii(int c) {
+    return (c >= 0 && c <= 127);
 }
 
+/* Splits the line into a token list (used to parse commands) */
 void tokenize(Token** head, char* line) {
     size_t line_length = strlen(line), i = 0, j;
     char* temp_tok;
@@ -189,6 +117,99 @@ void tokenize(Token** head, char* line) {
     }
 }
 
+/* ****************************** DYNAMIC MEMORY RELATED FUNCTIONS ****************************** */
+
+/* Returns a new token with default initialized values */
+Token* new_token() {
+    Token* token = malloc(sizeof(Token));
+    if (!token) {
+        on_error(MALLOC_ERR_MSG, true);
+    }
+    token->token_length = 0;
+    token->token = NULL;
+    token->next = NULL;
+    return token;
+}
+
+/* Free's the linked list defined by head */
+void free_list(Token* head) {
+    Token* ptr = head;
+    while(ptr) {
+        head = ptr;
+        ptr = ptr->next;
+        free(head->token);
+        free(head);
+    }
+}
+
+/* Malloc's and returns a ptr to string of size str_length in bytes */
+char* new_str(size_t str_length) {
+    char* str = malloc(str_length);
+    if (!str) {
+        on_error(MALLOC_ERR_MSG, false);
+        exit(1);
+    }
+    return str;
+}
+
+/* Increments the argument string's size in bytes by one. Requires null byte at the end of str! */
+void increment_str_size(char** str) {
+    size_t new_size = strlen(*str) + 2;
+    *str = realloc(*str, new_size);
+    if (!(*str)) {
+        on_error(MALLOC_ERR_MSG, false);
+        exit(1);
+    }
+}
+
+/* Adds the given token tok to list defined by head */
+void add_token(Token** head, Token* tok) {
+    if (*head == NULL) {
+        *head = tok;
+        return;
+    }
+    Token* ptr = *head;
+    while (ptr->next != NULL) {
+        ptr = ptr->next;
+    }
+    ptr->next = tok;
+}
+
+/* ****************************** FILE I/O FUNCTIONS ****************************** */
+
+/* Wrapper with error handling for fopen */
+FILE* open_file(char* filename, char* mode) {
+    FILE* fp = fopen(filename, mode);
+    if (!fp) {
+        on_error(OPEN_FILE_ERR_MSG, true);
+    }
+    return fp;
+}
+
+/* Reads a whole line from input_stream to line, returns false in case of EOF */
+bool get_line(char** line, FILE* input_stream) {
+    if (!input_stream) {
+        on_error(GET_LINE_ERR_MSG, true);
+    }
+    *line = NULL;
+    size_t size_of_line = 0;
+    ssize_t characters_read = getline(line, &size_of_line, input_stream);
+    if (characters_read == -1) {
+        if (ferror(input_stream) != 0) {
+            /* Global errno is set if getline fails and returns -1, also returns -1 on EOF.
+             According to man pages, *line should be free'd anyway. */
+            on_error(GET_LINE_ERR_MSG, false);
+            free(*line);
+            exit(1);
+        }
+        free(*line);
+        return false;
+    }
+    return true;
+}
+
+/* ****************************** SHELL MODES ****************************** */
+
 void interactive_mode() {
     char* line;
     Token* head = NULL;
@@ -199,14 +220,16 @@ void interactive_mode() {
         /* In either mode, if you hit the end-of-file marker (EOF), 
         you should call exit(0) and exit gracefully. */
         if (!get_line(&line, stdin)) exit(0);
+        /* validate user input */
         if (validate_input(line, &head)) {
-            print_list(head);
+            print_list(head); /* Test function */
             if (strcmp(head->token, BUILT_IN_EXIT) == 0) {
                 break;
             }
         } else {
             on_error(INV_INPUT_ERR_MSG, false);
         }
+        /* Line and its tokens need to be free'd after each command */
         free(line);
         free_list(head);
     }
